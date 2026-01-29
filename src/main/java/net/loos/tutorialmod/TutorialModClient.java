7e61db5767b2e0c22c6adb9e09fe5356594c06c3
lewzey <lmonu179@mtroyal.ca>
2026-01-29 09:02:23 -0700

    Initial commit

---
+++ src/main/java/net/loos/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.loos.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
