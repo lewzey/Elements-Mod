9feeeef751ac38faf6eb07f96bed7cb2d4f70035
lewzey <lmonu179@mtroyal.ca>
2026-01-29 20:12:59 -0700

    added loot drops and crafting recipe for gem block

--- src/main/java/net/loos/elementsmod/block/ModBlocks.java
+++ src/main/java/net/loos/elementsmod/block/ModBlocks.java
@@ -4,13 +4,13 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.loos.elementsmod.ElementsMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
+import net.minecraft.block.ExperienceDroppingBlock;import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.Identifier;import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block GRASS_GEM_BLOCK = registerBlock("grass_gem_block",
@@ -18,8 +18,10 @@ public class ModBlocks {
                     requiresTool().sounds(BlockSoundGroup.STONE)));
 
     public static final Block GRASS_GEM_ORE = registerBlock("grass_gem_ore",
-            new Block(AbstractBlock.Settings.create().strength(4f).
+            new ExperienceDroppingBlock(UniformIntProvider.create(2,5),
+                    AbstractBlock.Settings.create().strength(3f).
                     requiresTool().sounds(BlockSoundGroup.ROOTED_DIRT)));
+
 
 
     private static Block registerBlock (String name, Block block) {
