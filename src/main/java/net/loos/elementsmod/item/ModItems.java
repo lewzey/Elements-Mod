bbf334a2c5263a65aad06482ec07c0933deeff1e
jaden <jcowi267@mtroyal.ca>
2026-01-29 23:08:55 -0700

    started air class and added air ore and air bar. textures are final

--- src/main/java/net/loos/elementsmod/item/ModItems.java
+++ src/main/java/net/loos/elementsmod/item/ModItems.java
@@ -9,8 +9,20 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+
+    // GRASS
     public static final Item GRASS_GEM = registerItem("grass_gem",new Item(new Item.Settings()));
     public static final Item RAW_GRASS_GEM = registerItem("raw_grass_gem", new Item(new Item.Settings()));
+
+
+    // AIR
+    public static final Item AIR_BAR = registerItem("air_bar", new Item(new Item.Settings()));
+    public static final Item AIR_ORE = registerItem("air_ore", new Item(new Item.Settings()));
+
+    //FIRE
+
+
+    // WATER
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ElementsMod.MOD_ID, name), item);
     }
@@ -20,6 +32,10 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(GRASS_GEM);
             entries.add (RAW_GRASS_GEM);
+
+
+            entries.add(AIR_BAR);
+            entries.add(AIR_ORE);
         });
     }
 
