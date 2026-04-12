438ee24e49d33997beca53434e1b090b02189cad
Andres <andres.isturiz@icloud.com>
2026-04-12 17:22:55 -0600

    Ray's now follow if holding Aquabloom

--- src/main/java/net/loos/elementsmod/entity/custom/LuminousRayEntity.java
+++ src/main/java/net/loos/elementsmod/entity/custom/LuminousRayEntity.java
@@ -1,12 +1,15 @@
 package net.loos.elementsmod.entity.custom;
 
+import net.loos.elementsmod.item.ModItems;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.SwimAroundGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.SwimNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.WaterCreatureEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.world.World;
 
 public class LuminousRayEntity extends WaterCreatureEntity {
@@ -20,15 +23,23 @@ public class LuminousRayEntity extends WaterCreatureEntity {
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5);
     }
-
     @Override
     protected void initGoals() {
-        this.goalSelector.add(0, new SwimAroundGoal(this, 1.0, 10));
+        this.goalSelector.add(0, new EscapeDangerGoal(this, 1.25));
+
+        this.goalSelector.add(1, new TemptGoal(this, 1.2D,
+                Ingredient.ofItems(ModItems.AQUABLOOM), false));
+
+        this.goalSelector.add(2, new SwimAroundGoal(this, 1.0, 10));
+
+        this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(4, new LookAroundGoal(this));
     }
 
-    // This makes the mob actually use the "Swim" navigation instead of trying to walk on the seafloor
+
     @Override
     protected EntityNavigation createNavigation(World world) {
         return new SwimNavigation(this, world);
     }
 }
+
